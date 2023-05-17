fd1d517c2ed244b2b238de61ab76e159488398c0
Diludhz <mohammaddilshad0057@gmail.com>
2023-05-17 13:03:58 +0530

    commit

---
+++ bin/mathop.dart
@@ -0,0 +1,28 @@
+void add()
+{
+  int a=20,b=40;
+  print('addition=${a+b}');
+}
+void sub()
+{
+  int a=40,b=34;
+  print('substraction=${a-b}');
+}
+void mul()
+{
+  int a=6,b=90;
+  print('multiplication=${a*b}');
+}
+void div(){
+  int a=4,b=2;
+  print('division=${a/b}');
+}
+void main()
+{
+  add();
+  sub();
+  mul();
+  div();
+}
+
+
---
+++ bin/oprations.dart
@@ -0,0 +1,30 @@
+void main()
+{
+  dynamic a=100,b=70;
+  // print('a+b=${a+b}');
+  // print('a-b=${a-b}');
+  // print('a*b=${a*b}');
+  // print('a/b=${a/b}');
+  // print('a%b=${a%b}');
+  // print('a~/b=${a~/b}');
+  // print("a=b -> ${a=b}");
+  // print("a+=b -> ${a+=b}");
+  // print("a-=b -> ${a-=b}");
+  // print("a*=b -> ${a*=b}");
+  // print("a/=b -> ${a/=b}");
+  // print("a%=b -> ${a%=b}");
+  // print("a~/=b -> ${a~/=b}");
+  // int c=800 ,d=300;
+  // print('c  >  d  -> ${c>d}');
+  // print('c  <  d  -> ${c<d}');
+  // print('c  >=  d  -> ${c>=d}');
+  // print('c  <=  d  -> ${c<=d}');
+  // print('c  ==  d  -> ${c==d}');
+  // print('c  !=  d  -> ${c!=d}');
+String username="admin";
+String password="abc123";
+print(username=="abc"  &&  password=='abc123');
+
+
+
+}
